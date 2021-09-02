5544defde0e911087ec64a3419ef2b652678e99d
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-02 15:35:49 +0200

    add headers to index.html, add javascript function

--- javascript.js
+++ javascript.js
@@ -3,35 +3,63 @@ const bookmarksMain = document.querySelector('.bookmarks')
 const createMain = document.querySelector('.create')
 const profileMain = document.querySelector('.profile')
 
+const bookmarkButton = document.querySelector('.card__bookmark')
+
+const homeHeader = document.querySelector('.home-header')
+const bookmarksHeader = document.querySelector('.bookmarks-header')
+const createHeader = document.querySelector('.create-header')
+const profileHeader = document.querySelector('.profile-header')
+
 const homeButton = document.querySelector('#home-button')
 const bookmarksButton = document.querySelector('#bookmarks-button')
 const createButton = document.querySelector('#create-button')
 const profileButton = document.querySelector('#profile-button')
 
+function hideAll() {
+  const mainElArray = document.querySelectorAll('.main')
+
+  mainElArray.forEach((mainEl, index) => {
+    mainEl.classList.add('main--hidden')
+  })
+
+  const headerElArray = document.querySelectorAll('.header')
+
+  headerElArray.forEach((headerEl, index) => {
+    headerEl.classList.add('header--hidden')
+  })
+}
+
 homeButton.addEventListener('click', () => {
+  hideAll()
   homeMain.classList.remove('main--hidden')
-  bookmarksMain.classList.add('main--hidden')
-  createMain.classList.add('main--hidden')
-  profileMain.classList.add('main--hidden')
+  homeHeader.classList.remove('header--hidden')
 })
 
 bookmarksButton.addEventListener('click', () => {
-  homeMain.classList.add('main--hidden')
-  createMain.classList.add('main--hidden')
-  profileMain.classList.add('main--hidden')
+  hideAll()
   bookmarksMain.classList.remove('main--hidden')
+  bookmarksHeader.classList.remove('header--hidden')
 })
 
 createButton.addEventListener('click', () => {
-  homeMain.classList.add('main--hidden')
-  bookmarksMain.classList.add('main--hidden')
-  profileMain.classList.add('main--hidden')
+  hideAll()
   createMain.classList.remove('main--hidden')
+  createHeader.classList.remove('header--hidden')
 })
 
 profileButton.addEventListener('click', () => {
-  homeMain.classList.add('main--hidden')
-  bookmarksMain.classList.add('main--hidden')
-  createMain.classList.add('main--hidden')
+  hideAll()
   profileMain.classList.remove('main--hidden')
+  profileHeader.classList.remove('header--hidden')
+})
+
+// bookmarkButton
+
+// bookmarkButton.addEventListener('click', () => {
+//   bookmarkButton.classList.add('card__bookmark--bookmarked')
+// })
+
+bookmarkButton.addEventListener('click', function (e) {
+  const bookmarkToggle = e.bookmarkToggle
+  bookmarkToggle.classList.toggle('')
 })
