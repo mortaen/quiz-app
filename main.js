e95119bb7ec3373137cc7951ad8782b5c3a8eb0b
Morten Kolbe <kolbe.mor@gmail.com>
2021-09-08 18:55:23 +0200

    add array for card data + function

--- main.js
+++ main.js
@@ -9,16 +9,122 @@ const createHeader = document.querySelector('.create-header')
 const profileHeader = document.querySelector('.profile-header')
 const headerElArray = document.querySelectorAll('.header')
 
-const footerEl = document.querySelector('.footer')
-
-const cardElArray = document.querySelectorAll('.card')
-
-// navigation
-
 const homeButton = document.querySelector('#home-button')
 const bookmarksButton = document.querySelector('#bookmarks-button')
 const createButton = document.querySelector('#create-button')
 const profileButton = document.querySelector('#profile-button')
+
+const footerEl = document.querySelector('.footer')
+
+const cardElArray = document.querySelectorAll('.card')
+
+// Array for Cards
+
+const cardsDataArray = [
+  {
+    question: `Who was La Liga's Top Scorer in 2020/2021?`,
+    answer: 'Lionel Messi',
+    isBookmarked: false,
+    tags: ['Sports', 'Football', 'La Liga'],
+  },
+  {
+    question: 'How old was the oldest animal on earth?',
+    answer:
+      'A deep-sea sponge from the species Monorhaphis chuni lived to be 11000 years old.',
+    isBookmarked: false,
+    tags: ['Animals', 'Sea'],
+  },
+  {
+    question: 'Which bmx rider won the most X Games medals?',
+    answer: 'Dave Mirra won 24 X Games Medals over 19 appearances.',
+    isBookmarked: false,
+    tags: ['BMS', 'X Games', 'Sports'],
+  },
+  {
+    question: 'How many ants live on earth?',
+    answer:
+      'The number of ants on earth is estimated to be about 1 quadrillion spread out over more than 12000 ant species. That’s 1000000000000000 of these insects.',
+    isBookmarked: false,
+    tags: ['Animals', 'Science', 'Ants'],
+  },
+]
+
+//function to empty homeMain element
+
+function emptyHomeMain() {
+  while (homeMain.lastChild) {
+    homeMain.removeChild(homeMain.lastChild)
+  }
+}
+
+// empty homeMain on load
+
+homeMain.addEventListener('load', () => {
+  homeMain.innerHTML = ''
+  console.log('homeMain emptied')
+})
+
+// function to render array objects in html
+
+function renderCard(cardData) {
+  const cardSection = document.createElement('section')
+  cardSection.classList.add('card')
+  homeMain.appendChild(cardSection)
+
+  const cardBookmark = document.createElement('button')
+  cardBookmark.classList.add('card__bookmark', 'material-icons', 'md-48')
+  cardBookmark.ariaLabel = 'bookmark'
+  cardSection.appendChild(cardBookmark)
+
+  const cardQuestion = document.createElement('h2')
+  cardQuestion.classList.add('card__question')
+  cardQuestion.textContent = cardData.question
+  cardSection.appendChild(cardQuestion)
+
+  const cardButton = document.createElement('button')
+  cardButton.classList.add('card__answerbutton')
+  cardButton.textContent = 'show answer'
+  cardSection.appendChild(cardButton)
+
+  const cardAnswer = document.createElement('p')
+  cardAnswer.classList.add('card__answer')
+  cardAnswer.classList.add('card__answer--hidden')
+  cardAnswer.textContent = cardData.answer
+  cardSection.appendChild(cardAnswer)
+
+  const cardTags = document.createElement('ul')
+  cardTags.classList.add('tags')
+  cardSection.appendChild(cardTags)
+
+  cardData.tags.forEach(tag => {
+    const cardTag = document.createElement('li')
+    cardTag.classList.add('tags__item')
+    cardTag.textContent = tag
+    cardTags.appendChild(cardTag)
+  })
+}
+
+// renderCard(cardsDataArray[0])
+
+//render each card of cardsDataArray
+
+cardsDataArray.forEach(cardData => {
+  renderCard(cardData)
+})
+
+//empty homeMain on click and reload data from array
+
+homeButton.addEventListener('click', () => {
+  emptyHomeMain()
+  cardsDataArray.forEach(cardData => {
+    renderCard(cardData)
+  })
+  bookmarkElArray.forEach((bookmarkEl, index) => {
+    bookmarkEl.innerHTML = 'bookmark_border'
+  })
+})
+
+// navigation
 
 function hideAll() {
   const mainElArray = document.querySelectorAll('.main')
